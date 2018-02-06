8d343ecb5c4a1259d6ff831d6430508538ca4c3f
minhoShin95 <wolfox95@gmail.com>
2018-02-06 21:15:18 +0000

    more choice strategies added

--- Uncertain_Choices.R
+++ Uncertain_Choices.R
@@ -3,6 +3,10 @@ time_started <- sprintf('Started at: %s',Sys.time())
 real_dat <- read.csv("Speekenbrink_Konstantinidis_2016_Exp1_anonymous.csv")
 alldat <- subset(real_dat,id2 != 1) # excluding first participant. reindexing needed.
 
+LinearUtil <- function(R) {
+  R
+}
+
 ProspectUtil <- function(alpha,lambda,R) {
   ut <- vector("double",length=length(R))
   ut[R < 0] <- -lambda*abs(R[R<0])^alpha # only for the elements in vector R that are negative.
@@ -35,9 +39,7 @@ library(OpenMx)
 
 PMU <- function(E,S, sg2_epsilon) {
 
-  # fkf package used in Speekenbrink lab. 
-  
-  
+  # fkf package used in Speekenbrink lab.
   
   # Matrix A
   
@@ -85,6 +87,7 @@ P_BayesPMU_mLL <- function(par,data){
 
 PI <- function(E,S) {
   p <- pnorm((E - apply(E,1,max))/sqrt(S))
+  return(p)
 }
 
 P_BayesPI_mLL <- function(par,data){
@@ -103,9 +106,7 @@ P_BayesPI_mLL <- function(par,data){
 L_BayesPI_mLL <- function(par,data){
   sg2_zeta <- exp(par[1])
   sg2_epsilon <- exp(par[2])
-  alpha <- exp(par[3])
-  lambda <- exp(par[4])
-  ut <- ProspectUtil(alpha, lambda, data$payoff)
+  ut <- LinearUtil(data$payoff)
   res <- bayesUpdate(sg2_zeta,sg2_epsilon,data$id2,data$trial,data$deck,ut)
   E <- res[[1]]
   S <- res[[2]]
@@ -113,10 +114,78 @@ L_BayesPI_mLL <- function(par,data){
   -2*sum(log(p[cbind(1:nrow(data),data$deck)]))
 }
 
+
+# Upper Confidence Bound --------------------------------------------------
+
+UCB <- function(E,S) {
+  p <- E+1.96*sqrt(S)
+  return(p)
+}
+
+P_BayesUCB_mLL <- function(par,data){
+  sg2_zeta <- exp(par[1])
+  sg2_epsilon <- exp(par[2])
+  alpha <- exp(par[3])
+  lambda <- exp(par[4])
+  ut <- ProspectUtil(alpha, lambda, data$payoff)
+  res <- bayesUpdate(sg2_zeta,sg2_epsilon,data$id2,data$trial,data$deck,ut)
+  E <- res[[1]]
+  S <- res[[2]]
+  p <- UCB(E,S)
+  -2*sum(log(p[cbind(1:nrow(data),data$deck)]))
+}
+
+L_BayesUCB_mLL <- function(par,data){
+  sg2_zeta <- exp(par[1])
+  sg2_epsilon <- exp(par[2])
+  ut <- LinearUtil(data$payoff)
+  res <- bayesUpdate(sg2_zeta,sg2_epsilon,data$id2,data$trial,data$deck,ut)
+  E <- res[[1]]
+  S <- res[[2]]
+  p <- UCB(E,S)
+  -2*sum(log(p[cbind(1:nrow(data),data$deck)]))
+}
+
+
+# Expected Improvement ----------------------------------------------------
+
+EI <- function(E,S) {
+  m <- (E - apply(E,1,max))
+  p <- m*pnorm(m/sqrt(S))+sqrt(S)*dnorm(m/sqrt(S))
+  return(p)
+}
+
+
+P_BayesEI_mLL <- function(par,data){
+  sg2_zeta <- exp(par[1])
+  sg2_epsilon <- exp(par[2])
+  alpha <- exp(par[3])
+  lambda <- exp(par[4])
+  ut <- ProspectUtil(alpha, lambda, data$payoff)
+  res <- bayesUpdate(sg2_zeta,sg2_epsilon,data$id2,data$trial,data$deck,ut)
+  E <- res[[1]]
+  S <- res[[2]]
+  p <- EI(E,S)
+  -2*sum(log(p[cbind(1:nrow(data),data$deck)]))
+}
+
+L_BayesEI_mLL <- function(par,data){
+  sg2_zeta <- exp(par[1])
+  sg2_epsilon <- exp(par[2])
+  ut <- LinearUtil(data$payoff)
+  res <- bayesUpdate(sg2_zeta,sg2_epsilon,data$id2,data$trial,data$deck,ut)
+  E <- res[[1]]
+  S <- res[[2]]
+  p <- EI(E,S)
+  -2*sum(log(p[cbind(1:nrow(data),data$deck)]))
+}
+
+
+
 # Optimization ------------------------------------------------------------
 
 
-kalman_optfun <- function(x,llfun,lower,upper) { # for each participant, parameter estimation
+P_kalman_optfun <- function(x,llfun,lower,upper) { # for each participant, parameter estimation
   nstart <- 1000
   dat <- subset(alldat,id2==x)
   
@@ -138,9 +207,38 @@ kalman_optfun <- function(x,llfun,lower,upper) { # for each participant, paramet
   return(out)
 }
 
+L_kalman_optfun <- function(x,llfun,lower,upper) { # for each participant, parameter estimation
+  nstart <- 1000
+  dat <- subset(alldat,id2==x)
+  
+  # pars: sg_zeta, sg_epsilon, alpha, lambda
+  pars <- t(runif.sobol(nstart,length(lower),1,0,123345)) # t: matrix transpose # runif: generates random deviates # runif.sobol(n, dimension, init, scrambling, seed): Uniform scrambled Sobol sequence
+  pars <- t(lower + (upper - lower)*pars)
+  # pars[,1:2]: sg_zeta, sg_epsilon
+  pars[,1:2] <- log(pars[,1:2])
+
+  tll <- rep(NA,nstart)
+  for(i in 1:nstart) {
+    tll[i] <-  llfun(par=pars[i,],data=dat)
+  }
+  par <- pars[which.min(tll),]
+  out <- optim(par=par,fn=llfun,data=dat,control=list(maxit=1000)) # optim: general-purpose optimization. Nelder-Mead simplex algorithm.
+  cat("Subject",x,"estimated \n")
+  return(out)
+}
+
 library(parallelsugar) # parallel for Windows # source: https://github.com/nathanvan/parallelsugar
 library(fOptions)
 
-# BayesPMU <- mclapply(as.list(unique(alldat$id2)),kalman_optfun,llfun=P_BayesPMU_mLL,lower=c(.001,.001,.001,.001),upper=c(500,500,2,5),mc.preschedule=FALSE,mc.cores=4)
-BayesPI <- mclapply(as.list(unique(alldat$id2)),kalman_optfun,llfun=P_BayesPI_mLL,lower=c(.001,.001,.001,.001),upper=c(500,500,2,5),mc.preschedule=FALSE,mc.cores=4)
+# P_BayesPMU <- mclapply(as.list(unique(alldat$id2)),P_kalman_optfun,llfun=P_BayesPMU_mLL,lower=c(.001,.001,.001,.001),upper=c(500,500,2,5),mc.preschedule=FALSE,mc.cores=4)
+# P_BayesPI <- mclapply(as.list(unique(alldat$id2)),P_kalman_optfun,llfun=P_BayesPI_mLL,lower=c(.001,.001,.001,.001),upper=c(500,500,2,5),mc.preschedule=FALSE,mc.cores=4)
+# L_BayesPI <- mclapply(as.list(unique(alldat$id2)),L_kalman_optfun,llfun=L_BayesPI_mLL,lower=c(.001,.001),upper=c(500,500),mc.preschedule=FALSE,mc.cores=4)
+
+# P_BayesUCB <- mclapply(as.list(unique(alldat$id2)),P_kalman_optfun,llfun=P_BayesUCB_mLL,lower=c(.001,.001,.001,.001),upper=c(500,500,2,5),mc.preschedule=FALSE,mc.cores=4)
+L_BayesUCB <- mclapply(as.list(unique(alldat$id2)),L_kalman_optfun,llfun=L_BayesUCB_mLL,lower=c(.001,.001),upper=c(10,10),mc.preschedule=FALSE,mc.cores=4)
+
+# P_BayesEI <- mclapply(as.list(unique(alldat$id2)),P_kalman_optfun,llfun=P_BayesEI_mLL,lower=c(.001,.001,.001,.001),upper=c(500,500,2,5),mc.preschedule=FALSE,mc.cores=4)
+# L_BayesEI <- mclapply(as.list(unique(alldat$id2)),L_kalman_optfun,llfun=L_BayesEI_mLL,lower=c(.001,.001),upper=c(500,500),mc.preschedule=FALSE,mc.cores=4)
+
+
 time_finished <- sprintf('Ended at: %s',Sys.time())
